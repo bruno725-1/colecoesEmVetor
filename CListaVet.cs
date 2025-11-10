14dc82b38ff1f04a830978d236aa31bdccd25eaf
Bruno César Lima Araújo <bruuno.laraujo@gmail.com>
2025-11-10 10:49:13 -0300

    Classe CListaVet agora é public

--- CListaVet.cs
+++ CListaVet.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-class CListaVet<T> : IEnumerable<T>
+public class CListaVet<T> : IEnumerable<T>
 {
     private T[] Itens;
     private int Qtde = 0;
